af9fa1e6e2b10e0f7127ee16780f22fa0b6761dc
bk <brktmbrt@gmail.com>
2026-03-21 16:29:06 +0000

    update

--- store/apps.py
+++ store/apps.py
@@ -1,16 +1,6 @@
-#from django.apps import AppConfig
-
-
-#class StoreConfig(AppConfig):
-#    name = 'store'
-
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
-class MyAppConfig(AppConfig):
+
+class StoreConfig(AppConfig):
     name = 'store'
 
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "", "%%&%%Beki123")
