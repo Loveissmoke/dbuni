c0f0169958fc88eb75cca1df809236797c5035e5
bk <brktmbrt@gmail.com>
2026-03-22 09:17:39 +0000

    update

--- dbuni/settings.py
+++ dbuni/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
 # -------------------------
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # -------------------------
 # SECURITY
 # -------------------------
